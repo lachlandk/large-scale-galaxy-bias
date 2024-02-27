2e0b81726ab3c1ecbec54f63530a10a30ae0664c
Lachlan D-K <lachlandufort-kennett@outlook.com>
2024-02-27 23:47:10 +0000

    Added redshift bins

--- catalogues/create_catalogues.py
+++ catalogues/create_catalogues.py
@@ -9,17 +9,17 @@ if __name__ == "__main__":
 
     mag_limits = [19, 20, 21]
     mass_limits = [9.5, 10, 10.5, 11]
-    for lim in mag_limits:
-        print(f"Creating catalogue with magnitude limit {lim}... ")
-        catalogue = create_data_catalogue(lightcone_dir, files, mag_lim=lim, z_lims=[0.2, 0.5])
-        print(f"Created catalogue of size {np.sum(catalogue)}. Elapsed time: {datetime.now() - start_time}")
-        print("Plotting map...")
-        plot_catalogue(f"data_catalogue_r={lim}_m=0_z=0.2-0.5.hdf5", f"data_catalogue_r={lim}_m=0_z=0.2-0.5.png")
-        print(f"Map plotted, elapsed time: {datetime.now() - start_time}")
-    for lim in mass_limits:
-        print(f"Creating catalogue with mass limit {lim} log_10(M_sol/h)... ")
-        catalogue = create_data_catalogue(lightcone_dir, files, mass_lim=lim, z_lims=[0.2, 0.5])
-        print(f"Created catalogue of size {np.sum(catalogue)}. Elapsed time: {datetime.now() - start_time}")
-        print("Plotting map...")
-        plot_catalogue(f"data_catalogue_r=19.5_m={lim}_z=0.2-0.5.hdf5", f"data_catalogue_r=19.5_m={lim}_z=0.2-0.5.png")
-        print(f"Map plotted, elapsed time: {datetime.now() - start_time}")
+    z_bins = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    for i in range(len(z_bins) - 1):
+        print(f"Creating catalouges in redshift range z={z_bins[i]}-{z_bins[i+1]}...")
+        for lim in mag_limits:
+            print(f"Creating catalogue with magnitude limit {lim}... ")
+            catalogue = create_data_catalogue(lightcone_dir, files, mag_lim=lim, z_lims=[z_bins[i], z_bins[i+1]])
+            print(f"Created catalogue of size {np.sum(catalogue)}. Elapsed time: {datetime.now() - start_time}")
+        for lim in mass_limits:
+            print(f"Creating catalogue with mass limit {lim} log_10(M_sol/h)... ")
+            catalogue = create_data_catalogue(lightcone_dir, files, mass_lim=lim, z_lims=[z_bins[i], z_bins[i+1]])
+            print(f"Created catalogue of size {np.sum(catalogue)}. Elapsed time: {datetime.now() - start_time}")
+        print(f"Creating random catalogue for redshift range z={z_bins[i]}-{z_bins[i+1]}...")
+        create_random_catalogue(1000000, f"data_catalogue_r=19_m=0_z={z_bins[i]}-{z_bins[i+1]}.hdf5", f"random_catalogue_1_000_000_z={z_bins[i]}-{z_bins[i+1]}.hdf5")
+        print(f"Created catalouges in redshift range z={z_bins[i]}-{z_bins[i+1]}. Elapsed time: {datetime.now() - start_time}")
