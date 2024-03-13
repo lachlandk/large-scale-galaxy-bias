5bc22ffeee89bad9d1e7b3ef015937768698dc63
Lachlan D-K <lachlandufort-kennett@outlook.com>
2024-03-13 12:59:59 +0000

    Updated catalogue creation script

--- catalogues/create_catalogues.py
+++ catalogues/create_catalogues.py
@@ -1,25 +1,31 @@
-import numpy as np
 from datetime import datetime
 from catalogue import *
 
 if __name__ == "__main__":
     start_time = datetime.now()
-    lightcone_dir = "/freya/ptmp/mpa/vrs/TestRuns/MTNG/MTNG-L500-2160-A/SAM/galaxies_lightcone_01/"
     files = 155
+    z_bins = [0, 0.03, 0.06, 0.1, 0.2, 0.3, 1]
+    mag_limits = [17, 18, 19]
+    random_catalogue_size = 500000
 
-    mag_limits = [19, 20, 21]
-    mass_limits = [9.5, 10, 10.5, 11]
-    z_bins = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
-    for i in range(len(z_bins) - 1):
-        print(f"Creating catalouges in redshift range z={z_bins[i]}-{z_bins[i+1]}...")
-        for lim in mag_limits:
-            print(f"Creating catalogue with magnitude limit {lim}... ")
-            catalogue = create_data_catalogue(lightcone_dir, files, mag_lim=lim, z_lims=[z_bins[i], z_bins[i+1]])
-            print(f"Created catalogue of size {np.sum(catalogue)}. Elapsed time: {datetime.now() - start_time}")
-        for lim in mass_limits:
-            print(f"Creating catalogue with mass limit {lim} log_10(M_sol/h)... ")
-            catalogue = create_data_catalogue(lightcone_dir, files, mass_lim=lim, z_lims=[z_bins[i], z_bins[i+1]])
-            print(f"Created catalogue of size {np.sum(catalogue)}. Elapsed time: {datetime.now() - start_time}")
-        print(f"Creating random catalogue for redshift range z={z_bins[i]}-{z_bins[i+1]}...")
-        create_random_catalogue(1000000, f"data_catalogue_r=19_m=0_z={z_bins[i]}-{z_bins[i+1]}.hdf5", f"random_catalogue_1_000_000_z={z_bins[i]}-{z_bins[i+1]}.hdf5")
-        print(f"Created catalouges in redshift range z={z_bins[i]}-{z_bins[i+1]}. Elapsed time: {datetime.now() - start_time}")
+    print("Creating catalogues from the A realisation...")
+    lightcone_dir = "/freya/ptmp/mpa/vrs/TestRuns/MTNG/MTNG-L500-2160-A/SAM/galaxies_lightcone_01/"
+    for lim in mag_limits:
+        print(f"Creating data catalogue with magnitude limit r<{lim}")
+        create_data_catalogue(lightcone_dir, files, z_bins, mag_lim=lim, save_name=f"data_r<{lim}_A.hdf5")
+        print(f"Galaxy catalogue created, elapsed time: {datetime.now() - start_time}")
+        print(f"Creating random catalogue of size {random_catalogue_size}...")
+        create_random_catalogue(random_catalogue_size, f"data_r<{lim}_A.hdf5", f"random_r<{lim}_A.hdf5")
+        print(f"Random catalogue created, elapsed time: {datetime.now() - start_time}")
+    print("Finished creating catalogues from the A realisation.")
+
+    print("Creating catalogues from the B realisation...")
+    lightcone_dir = "/freya/ptmp/mpa/vrs/TestRuns/MTNG/MTNG-L500-2160-B/SAM/galaxies_lightcone_01/"
+    for lim in mag_limits:
+        print(f"Creating data catalogue with magnitude limit r<{lim}")
+        create_data_catalogue(lightcone_dir, files, z_bins, mag_lim=lim, save_name=f"data_r<{lim}_B.hdf5")
+        print(f"Galaxy catalogue created, elapsed time: {datetime.now() - start_time}")
+        print(f"Creating random catalogue of size {random_catalogue_size}...")
+        create_random_catalogue(random_catalogue_size, f"data_r<{lim}_B.hdf5", f"random_r<{lim}_B.hdf5")
+        print(f"Random catalogue created, elapsed time: {datetime.now() - start_time}")
+    print("Finished creating catalogues from the B realisation.")
