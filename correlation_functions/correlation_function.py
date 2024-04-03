a83dd32a2096c18e849e46af507b21245af72cf3
Lachlan D-K <lachlandufort-kennett@outlook.com>
2024-04-03 23:45:00 +0100

    Changed script to output only 1 file containing all pair counts

--- correlation_functions/correlation_function.py
+++ correlation_functions/correlation_function.py
@@ -2,6 +2,7 @@ import h5py
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
+# from scipy import interpolate, integrate
 from Corrfunc.mocks.DDsmu_mocks import DDsmu_mocks
 
 # cosmological parameters
@@ -11,113 +12,110 @@ h = 0.6774
 def correlation_function(data_file, catalogue, s_bins):
     start_time = datetime.now()
 
-    # count pairs
-    # calculate_flags = {"DD": False, "RR": False, "DR": False}
-    # filename = f"correlation_functions/corrfunc_{data_catalogue.split('.')[0]}_{random_catalogue.split('.')[0]}_nmu_bins={nmu_bins}.hdf5"
-    # try:
-    #     with h5py.File(filename, "r") as pairs:
-    #         for dataset in ("DD", "RR", "DR"):
-    #             try:
-    #                 for z_bin in pairs:
-    #                     z_bins.append(z_bin)
-    #                     DD_pairs.append(np.array(pairs[z_bin][dataset]))
-    #             except KeyError:
-    #                 calculate_flags[dataset] = True
-    # except FileNotFoundError:
-    #     with h5py.File(filename, "w") as pairs:
-    #         print("Counting data pairs...")
-
     nmu_bins = 100
+    filename = f"correlation_functions/corrfunc_{data_file}"
+    with h5py.File(filename, "a") as save_file:
+        if f"{catalogue}/xi_0" in save_file:
+            xi = np.array(save_file[catalogue]["xi_0"])
+            sigma = np.array(save_file[catalogue]["sigma"])
+            print(f"Correlation function loaded, elapsed time: {datetime.now() - start_time}")
+        else:
+            with h5py.File(f"catalogues/{data_file}", "r") as sample:
+                d_pos = np.array(sample[catalogue]["Pos"])
+                d_ra = d_pos[:,0]
+                d_dec = d_pos[:,1]
+                d_dist = d_pos[:,2] / h
+                r_pos = np.array(sample[f"{catalogue}/random"]["Pos"])
+                r_ra = r_pos[:,0]
+                r_dec = r_pos[:,1]
+                r_dist = r_pos[:,2] / h
 
-    with h5py.File(f"catalogues/{data_file}", "r") as sample:
-        d_pos = np.array(sample[catalogue]["Pos"])
-        d_ra = d_pos[:,0]
-        d_dec = d_pos[:,1]
-        d_dist = d_pos[:,2] / h
-        r_pos = np.array(sample[f"{catalogue}/random"]["Pos"])
-        r_ra = r_pos[:,0]
-        r_dec = r_pos[:,1]
-        r_dist = r_pos[:,2] / h
-
-        print(f"Counting data pairs, sample size: {d_pos.shape[0]}...")
-        filename = f"correlation_functions/DD_pair_counts_{data_file}"
-        try:
-            with h5py.File(filename, "r") as pairs:
-                DD_pairs = np.array(pairs[catalogue]["DD"])
-        except (FileNotFoundError, KeyError):
-            with h5py.File(filename, "a") as pairs:
-                save = pairs.create_group(catalogue)
+            # count data pairs
+            print(f"Counting data pairs, sample size: {d_pos.shape[0]}...")
+            if f"{catalogue}/DD" in save_file:    
+                DD_pairs = np.array(save_file[catalogue]["DD"])
+                N_D = save_file[catalogue].attrs["N_D"]
+            else:
+                save = save_file.create_group(catalogue)
                 if d_pos.shape[0] < 2:
                     DD_pairs = np.zeros((len(s_bins) - 1) * nmu_bins)
+                    N_D = 0
                 else:
-                    DD_pairs = 1/(d_pos.shape[0]**2) * DDsmu_mocks(autocorr=True, cosmology=2, nthreads=16, mu_max=1, nmu_bins=nmu_bins, binfile=s_bins, RA1=d_ra, DEC1=d_dec, CZ1=d_dist, is_comoving_dist=True)["npairs"]
+                    DD_pairs = DDsmu_mocks(autocorr=True, cosmology=2, nthreads=16, mu_max=1, nmu_bins=nmu_bins, binfile=s_bins, RA1=d_ra, DEC1=d_dec, CZ1=d_dist, is_comoving_dist=True)["npairs"]
+                    N_D = d_pos.shape[0]
                 save.create_dataset("DD", data=DD_pairs)
-        print(f"Data pairs counted, elapsed time: {datetime.now() - start_time}")
-
-        print(f"Counting random pairs, sample size: {r_pos.shape[0]}...")
-        filename = f"correlation_functions/RR_pair_counts_{data_file}"
-        try:
-            with h5py.File(filename, "r") as pairs:
-                RR_pairs = np.array(pairs[catalogue]["RR"])
-        except (FileNotFoundError, KeyError):
-            with h5py.File(filename, "a") as pairs:
-                save = pairs.create_group(catalogue)
+                save.attrs["N_D"] = N_D
+            print(f"Data pairs counted, elapsed time: {datetime.now() - start_time}")
+    
+            # count random pairs
+            print(f"Counting random pairs, sample size: {r_pos.shape[0]}...")
+            if f"{catalogue}/RR" in save_file:
+                RR_pairs = np.array(save_file[catalogue]["RR"])
+                N_R = save_file[catalogue].attrs["N_R"]
+            else:
                 if r_pos.shape[0] < 2:
                     RR_pairs = np.zeros((len(s_bins) - 1) * nmu_bins)
+                    N_R = 0
                 else:
-                    RR_pairs = 1/(r_pos.shape[0]**2) * DDsmu_mocks(autocorr=True, cosmology=2, nthreads=16, mu_max=1, nmu_bins=nmu_bins, binfile=s_bins, RA1=r_ra, DEC1=r_dec, CZ1=r_dist, is_comoving_dist=True)["npairs"]
-                save.create_dataset("RR", data=RR_pairs)
-        print(f"Random pairs counted, elapsed time: {datetime.now() - start_time}")
+                    RR_pairs = DDsmu_mocks(autocorr=True, cosmology=2, nthreads=16, mu_max=1, nmu_bins=nmu_bins, binfile=s_bins, RA1=r_ra, DEC1=r_dec, CZ1=r_dist, is_comoving_dist=True)["npairs"]
+                    N_R = r_pos.shape[0]
+                save_file[catalogue].create_dataset("RR", data=RR_pairs)
+                save_file[catalogue].attrs["N_R"] = r_pos.shape[0]
+            print(f"Random pairs counted, elapsed time: {datetime.now() - start_time}")
 
-        print("Counting data-random pairs...")
-        filename = f"correlation_functions/DR_pair_counts_{data_file}"
-        try:
-            with h5py.File(filename, "r") as pairs:
-                mu = pairs[catalogue].attrs["mu"]
-                DR_pairs = np.array(pairs[catalogue]["DR"])
-        except (FileNotFoundError, KeyError):
-            with h5py.File(filename, "a") as pairs:
-                save = pairs.create_group(catalogue)
+            # count data-random pairs
+            print("Counting data-random pairs...")
+            if f"{catalogue}/DR" in save_file:
+                mu = save_file[catalogue].attrs["mu"]
+                DR_pairs = np.array(save_file[catalogue]["DR"])
+            else:
                 if d_pos.shape[0] < 2:
                     DR_pairs = np.zeros((len(s_bins) - 1) * nmu_bins)
                     mu = np.linspace(0, 1, nmu_bins)
                 else:
-                    count = DDsmu_mocks(autocorr=False, cosmology=2, nthreads=16, mu_max=1, nmu_bins=nmu_bins, binfile=s_bins, RA1=d_ra, DEC1=d_dec, CZ1=d_dist, RA2=r_ra, DEC2=r_dec, CZ2=r_dist, is_comoving_dist=True)
-                    mu = count["mumax"][0:nmu_bins]
-                    DR_pairs = (1/(d_pos.shape[0] * r_pos.shape[0])) * count["npairs"]
-                save.create_dataset("DR", data=DR_pairs)
-                save.attrs["ndata"] = d_pos.shape[0]
-                save.attrs["nrandom"] = r_pos.shape[0]
-                save.attrs["mu"] = mu
-        print(f"Data-random pairs counted, elapsed time: {datetime.now() - start_time}")
+                    DR_pairs_struct = DDsmu_mocks(autocorr=False, cosmology=2, nthreads=16, mu_max=1, nmu_bins=nmu_bins, binfile=s_bins, RA1=d_ra, DEC1=d_dec, CZ1=d_dist, RA2=r_ra, DEC2=r_dec, CZ2=r_dist, is_comoving_dist=True)
+                    DR_pairs = DR_pairs_struct["npairs"]
+                    mu = DR_pairs_struct["mumax"][0:nmu_bins]
+                save_file[catalogue].create_dataset("DR", data=DR_pairs)
+                save_file[catalogue].attrs["mu"] = mu
+            print(f"Data-random pairs counted, elapsed time: {datetime.now() - start_time}")
 
-    # calculate correlation function
-    print("Calculating correlation function...")
-    corrfunc = (DD_pairs - 2*DR_pairs + RR_pairs) / RR_pairs
-    xi = np.ndarray((5, len(s_bins) - 1))
-    for i in range(len(s_bins) - 1):
-        j = nmu_bins*i
-        xi[0, i] = np.trapz(corrfunc[j:j+nmu_bins], mu)  # monopole
-        xi[1, i] = 3 * np.trapz(corrfunc[j:j+nmu_bins] * mu, mu)  # dipole
-        xi[2, i] = 5 * np.trapz(corrfunc[j:j+nmu_bins] * 0.5 * (3*mu**2 - 1), mu)  # quadrupole
-        xi[3, i] = 7 * np.trapz(corrfunc[j:j+nmu_bins] * 0.5 * (5*mu**3 - 3*mu), mu)  # octupole
-        xi[4, i] = 9 * np.trapz(corrfunc[j:j+nmu_bins] * 0.125 * (35*mu**4 - 30*mu**2 + 3), mu)  # hexadecapole
-    print(f"Correction function calculated, elapsed time: {datetime.now() - start_time}")
+            # calculate correlation function
+            print("Calculating correlation function...")
+            DD = DD_pairs / (N_D*(N_D - 1))
+            RR = RR_pairs / (N_R*(N_R - 1))
+            DR = DR_pairs / (N_D * N_R)
+            corrfunc = (DD - 2*DR + RR) / RR
+            xi = np.ndarray(len(s_bins) - 1)
+            sigma = np.ndarray(len(s_bins) - 1)
+            for i in range(len(s_bins) - 1):
+                j = nmu_bins * i
+                xi[i] = np.trapz(corrfunc[j:j+nmu_bins], mu)  # monopole
+                # n = N_D * 6/(np.pi * (s_bins[i+1]**3 - s_bins[i]**3))
+                # xi_interp = interpolate.CubicSpline(np.concatenate(([0], s_bins[:i+1])), np.concatenate(([0], xi[:i+1])))
+                # J_3 = integrate.quad(lambda r: xi_interp(r) * r**2, 0, s_bins[i])[0]
+                # sigma[i] = (1 + xi[i]) * (1 + 4*np.pi*n*J_3) / np.sqrt(np.trapz(DD_pairs[j:j+nmu_bins], mu))
+                sigma[i] = (1 + xi[i]) / np.sqrt(np.trapz(DD_pairs[j:j+nmu_bins], mu))
+            save_file[catalogue].create_dataset("s", data=s_bins[:-1])
+            save_file[catalogue].create_dataset("xi_0", data=xi)
+            save_file[catalogue].create_dataset("sigma", data=sigma)
+            print(f"Correction function calculated, elapsed time: {datetime.now() - start_time}")
 
-    return xi
+    return xi, sigma
 
 
 if __name__ == "__main__":
-    s = np.linspace(0.1, 200, 101)
+    s = np.linspace(0.1, 100, 101)
 
-    xi_A = correlation_function("magnitude_limited_A.hdf5", "0.0<z<0.2", s)
-    xi_B = correlation_function("magnitude_limited_B.hdf5", "0.0<z<0.2", s)
-    xi = [(xi_A_bin + xi_B_bin) / 2 for xi_A_bin, xi_B_bin in zip(xi_A, xi_B)]
+    xi_A, sigma_A = correlation_function("magnitude_limited_A.hdf5", "0.0<z<0.2", s)
+    xi_B, sigma_B = correlation_function("magnitude_limited_B.hdf5", "0.0<z<0.2", s)
+    xi = np.mean([xi_A, xi_B], axis=0)
+    sigma = np.mean([sigma_A, sigma_B], axis=0)
 
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
     fig.suptitle("Correlation Function in Real Space")
 
-    ax.plot(s[:-1], s[:-1]**2 * xi[0])
+    ax.plot(s[:-1], s[:-1]**2 * xi)
     ax.set_xlabel("$r$ [cMpc]")
     ax.set_ylabel("$r^2\\xi(r)$ [cMpc$^2$]")
     
