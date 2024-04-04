Added calculation for median redshift, correct error in variance formula

correlation_functions/correlation_function.py
@@ -25,6 +25,7 @@ def correlation_function(data_file, catalogue, s_bins):
                 d_ra = d_pos[:,0]
                 d_dec = d_pos[:,1]
                 d_dist = d_pos[:,2] / h
+                z_cos = np.array(sample[catalogue]["CosZ"])
                 r_pos = np.array(sample[f"{catalogue}/random"]["Pos"])
                 r_ra = r_pos[:,0]
                 r_dec = r_pos[:,1]
@@ -66,8 +67,9 @@ def correlation_function(data_file, catalogue, s_bins):
             # count data-random pairs
             print("Counting data-random pairs...")
             if f"{catalogue}/DR" in save_file:
-                mu = save_file[catalogue].attrs["mu"]
                 DR_pairs = np.array(save_file[catalogue]["DR"])
+                mu = save_file[catalogue].attrs["mu"]
+                median_z = save_file[catalogue].attrs["median_z"]
             else:
                 if d_pos.shape[0] < 2:
                     DR_pairs = np.zeros((len(s_bins) - 1) * nmu_bins)
@@ -76,8 +78,10 @@ def correlation_function(data_file, catalogue, s_bins):
                     DR_pairs_struct = DDsmu_mocks(autocorr=False, cosmology=2, nthreads=16, mu_max=1, nmu_bins=nmu_bins, binfile=s_bins, RA1=d_ra, DEC1=d_dec, CZ1=d_dist, RA2=r_ra, DEC2=r_dec, CZ2=r_dist, is_comoving_dist=True)
                     DR_pairs = DR_pairs_struct["npairs"]
                     mu = DR_pairs_struct["mumax"][0:nmu_bins]
+                median_z = np.median(z_cos)
                 save_file[catalogue].create_dataset("DR", data=DR_pairs)
                 save_file[catalogue].attrs["mu"] = mu
+                save_file[catalogue].attrs["median_z"] = median_z
             print(f"Data-random pairs counted, elapsed time: {datetime.now() - start_time}")
 
             # calculate correlation function
@@ -95,7 +99,7 @@ def correlation_function(data_file, catalogue, s_bins):
                 # xi_interp = interpolate.CubicSpline(np.concatenate(([0], s_bins[:i+1])), np.concatenate(([0], xi[:i+1])))
                 # J_3 = integrate.quad(lambda r: xi_interp(r) * r**2, 0, s_bins[i])[0]
                 # sigma[i] = (1 + xi[i]) * (1 + 4*np.pi*n*J_3) / np.sqrt(np.trapz(DD_pairs[j:j+nmu_bins], mu))
-                sigma[i] = (1 + xi[i]) / np.sqrt(np.trapz(DD_pairs[j:j+nmu_bins], mu))
+                sigma[i] = np.sqrt((1 + xi[i]) / np.trapz(DD_pairs[j:j+nmu_bins], mu))
             save_file[catalogue].create_dataset("s", data=s_bins[:-1])
             save_file[catalogue].create_dataset("xi_0", data=xi)
             save_file[catalogue].create_dataset("sigma", data=sigma)