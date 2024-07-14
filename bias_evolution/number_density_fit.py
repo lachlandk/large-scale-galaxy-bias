3d4992294f5e33e1e41178224ac4dd943c5bbf96
Lachlan D-K <lachlandufort-kennett@outlook.com>
2024-07-14 12:35:16 +0100

    Added stronger priors for fitting number density

--- bias_evolution/number_density_fit.py
+++ bias_evolution/number_density_fit.py
@@ -1,20 +1,21 @@
 import h5py
 import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 
-from mcmc_fit import mcmc, plot_model, plot_posterior_1d, plot_chains
+from mcmc_fit import mcmc, plot_model, plot_chains, plot_corner
 from bias_models import n_g as number_density
 
 
 def number_density_evolution(theta, args):
     z_star, sigma_0, alpha_1, alpha_2 = theta
     z, n_g, _ = args
-    return number_density(z, n_g[0], z_star, sigma_0, alpha_1, alpha_2)
+    return number_density(z, n_g[np.argmax(z)], z_star, sigma_0, alpha_1, alpha_2)
 
 
 def number_density_log_priors(theta):
     z_star, sigma_0, alpha_1, alpha_2 = theta
-    if z_star > 0:
+    if z_star > 0 and z_star < 3 and sigma_0 > 0 and sigma_0 < 1 and alpha_1 < 1 and alpha_2 < 1:
         return 0.0
     return -np.inf
 
@@ -35,10 +36,11 @@ def fit_number_density_evolution(catalogue, subsample, nwalkers, total_steps, bu
             posterior = np.array(number_density_save[catalogue][subsample]["posterior"])
             chains = np.array(number_density_save[catalogue][subsample]["chains"])
         except (ValueError, KeyError):
-            initial = (0.5, 0.1, 1, 1)
+            initial = (0.3, 0.5, 0.01, -0.001)
+            # initial = np.clip(np.random.default_rng().normal((0.3, 0.5, 0.01, -0.001), (0.01, 0.025, 0.0005, 0.00005), (nwalkers, 4)), (0, 0, -np.inf, -np.inf), (3, 1, 1, 1))
             posterior, chains = mcmc(number_density_evolution, number_density_log_priors, (z, n_g, sigma_n_g), initial, nwalkers, 4, total_steps, burn_in_steps)
-            params = np.mean(posterior, axis=0)
-            sigma_params = np.std(posterior, axis=0)
+            params = np.median(posterior, axis=0)
+            sigma_params = stats.median_abs_deviation(posterior, axis=0)
 
             # save optimal values
             group = f"{catalogue}/{subsample}" if subsample != "." else catalogue
@@ -54,34 +56,57 @@ def fit_number_density_evolution(catalogue, subsample, nwalkers, total_steps, bu
     ax.set_xlabel("$z$")
     ax.set_ylabel("$n_g$")
     model_fig.suptitle(f"Number density evolution: {catalogue}/{subsample}")
-    # ax.annotate(f"$b_{{1,i}}={np.round(b_1_i, decimals=2)}\\pm{np.round(sigma_b_1_i, decimals=2)}$", (0.05, 0.05), xycoords="axes fraction", fontsize=15)
+    ax.annotate(f"$z_\\ast={params[0]}\\pm{sigma_params[0]}$", (0.05, 0.95), xycoords="axes fraction", fontsize=15)
+    ax.annotate(f"$\\sigma_0={params[1]}\\pm{sigma_params[1]}$", (0.05, 0.9), xycoords="axes fraction", fontsize=15)
+    ax.annotate(f"$\\alpha_1={params[2]}\\pm{sigma_params[2]}$", (0.05, 0.85), xycoords="axes fraction", fontsize=15)
+    ax.annotate(f"$\\alpha_2={params[3]}\\pm{sigma_params[3]}$", (0.05, 0.8), xycoords="axes fraction", fontsize=15)
 
-    # # plot posterior and chains as insets
-    # posterior_inset = ax.inset_axes([0.65, 0.65, 0.3, 0.3])
-    # plot_posterior_1d(posterior_inset, posterior, 20)
-    # posterior_inset.set_xlabel("$b_{1,i}$")
-    # posterior_inset.set_ylabel("$p(b_{1,i}|b_1(z))$")
-    # chains_inset = ax.inset_axes([0.1, 0.75, 0.4, 0.2])
-    # plot_chains(chains_inset, chains, burn_in_steps)
-    # chains_inset.set_xlabel("Steps")
-    # chains_inset.set_ylabel("$b_{1,i}$")
+    # clip posterior distribution to ignore outliers
+    clipped_posterior = np.clip(posterior, params - 5*sigma_params, params + 5*sigma_params)
+
+    # plot posterior as corner plot
+    corner_fig, axes = plt.subplots(4, 4, figsize=(10, 10), layout="constrained")
+    plot_corner(axes, clipped_posterior, 20)
+    axes[3, 0].set_xlabel("$z_\\ast$")
+    axes[3, 1].set_xlabel("$\\sigma_0$")
+    axes[3, 2].set_xlabel("$\\alpha_1$")
+    axes[3, 3].set_xlabel("$\\alpha_2$")
+    axes[1, 0].set_ylabel("$\\sigma_0$")
+    axes[2, 0].set_ylabel("$\\alpha_1$")
+    axes[3, 0].set_ylabel("$\\alpha_2$")
+    
+    # plot chains
+    chains_fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 10), sharex=True, layout="constrained")
+    plot_chains(ax1, chains, burn_in_steps, param_index=0)
+    ax1.set_ylabel("$z_\\ast$")
+    plot_chains(ax2, chains, burn_in_steps, param_index=1)
+    ax2.set_ylabel("$\\sigma_0$")
+    plot_chains(ax3, chains, burn_in_steps, param_index=2)
+    ax3.set_ylabel("$\\alpha_1$")
+    plot_chains(ax4, chains, burn_in_steps, param_index=3)
+    ax4.set_ylabel("$\\alpha_2$")
+    ax4.set_xlabel("Steps")
 
     if subsample == ".":
         model_fig.savefig(f"bias_evolution/number_density_evolution_{catalogue}.pdf")
+        corner_fig.savefig(f"bias_evolution/number_density_evolution_{catalogue}_corner.pdf")
+        chains_fig.savefig(f"bias_evolution/number_density_evolution_{catalogue}_chains.pdf")
     else:
         model_fig.savefig(f"bias_evolution/number_density_evolution_{catalogue}_{subsample.replace('<', '_lt_').replace('.', '_')}.pdf")
+        corner_fig.savefig(f"bias_evolution/number_density_evolution_{catalogue}_{subsample.replace('<', '_lt_').replace('.', '_')}_corner.pdf")
+        chains_fig.savefig(f"bias_evolution/number_density_evolution_{catalogue}_{subsample.replace('<', '_lt_').replace('.', '_')}_chains.pdf")
 
 
 if __name__ == "__main__":
-    print("Fitting number density evolution for constant number density sample")
-    fit_number_density_evolution("const_number_density", ".", 64, 5000, 100)
+    # print("Fitting number density evolution for constant number density sample")
+    # fit_number_density_evolution("const_number_density", ".", 128, 20000, 1000)
 
     print("Fitting number density evolution for constant stellar mass (high) sample")
-    fit_number_density_evolution("const_stellar_mass", "11.5<m<inf", 64, 5000, 100)
+    fit_number_density_evolution("const_stellar_mass", "11.5<m<inf", 256, 10000, 1000)
     print("Fitting number density evolution for constant stellar mass (medium) sample")
-    fit_number_density_evolution("const_stellar_mass", "11<m<11.5", 64, 5000, 100)
+    fit_number_density_evolution("const_stellar_mass", "11<m<11.5", 256, 10000, 1000)
     print("Fitting number density evolution for constant stellar mass (low) sample")
-    fit_number_density_evolution("const_stellar_mass", "10.5<m<11", 64, 5000, 100)
+    fit_number_density_evolution("const_stellar_mass", "10.5<m<11", 256, 10000, 1000)
 
     print("Fitting number density evolution for magnitude limited sample")
-    fit_number_density_evolution("magnitude_limited", ".", 64, 5000, 100)
+    fit_number_density_evolution("magnitude_limited", ".", 256, 10000, 1000)
