8af5ad7788ff30774c6a419c8e53591387dd5bee
Lachlan D-K <lachlandufort-kennett@outlook.com>
2024-08-14 18:48:59 +0100

    Removed angular size limits for samples, removed random catalogue creation

--- catalogues/magnitude_limited.py
+++ catalogues/magnitude_limited.py
@@ -1,45 +1,30 @@
 from datetime import datetime
-from catalogue import *
+from catalogue import select_galaxies
 
 
-def create_catalogue(lightcone_dir, files, filename, multiplier):
+def create_catalogue(lightcone_dir, files, filename):
     z_bins = [(0.74, 0.8), (0.68, 0.74), (0.62, 0.68), (0.56, 0.62), (0.5, 0.56), (0.44, 0.5), (0.38, 0.44), (0.32, 0.38), (0.26, 0.32), (0.2, 0.26)]
-    flags = [5, 6]
-    flags_2 = [7, 8, 9, 10]
 
-    flag = 1
     for low_z, high_z in z_bins:
-        if flag in flags:
-            ra_lims = (30, 60)
-            dec_lims = (30, 60)
-        elif flag in flags_2:
-            ra_lims = (40, 60)
-            dec_lims = (40, 60)
-        else:
-            ra_lims = (0, 90)
-            dec_lims = (0, 90)
-        total_galaxies, number_density, median_z = select_galaxies(lightcone_dir, files, filename, f"{low_z}<z<{high_z}", z_lims=(low_z, high_z), dec_lims=dec_lims, ra_lims=ra_lims)
-        create_random_catalogue(multiplier, filename, f"{low_z}<z<{high_z}")
-        print(f"Number of galaxies in the range {low_z}<z<{high_z}{' (cut sky)' if flag in flags else ' (super cut sky)' if flag in flags_2 else ''}: {total_galaxies}")
-        print(f"Galaxy number density: {number_density}/Mpc^3")
+        total_galaxies, number_density, median_z = select_galaxies(lightcone_dir, files, filename, f"{low_z}<z<{high_z}", z_lims=(low_z, high_z))
+        print(f"Number of galaxies in the range {low_z}<z<{high_z}: {total_galaxies}")
+        print(f"Galaxy number density: {number_density}/cMpc^3")
         print(f"Median cosmological redshift: {median_z}")
-        flag += 1
 
 
 if __name__ == "__main__":
     files = 155
-    multiplier = 50  # how many times bigger should the random catalogue be
     
     start_time = datetime.now()
     lightcone_dir = "/freya/ptmp/mpa/vrs/TestRuns/MTNG/MTNG-L500-2160-A/SAM/galaxies_lightcone_01"
     filename = "magnitude_limited_A.hdf5"
     print("Creating magnitude limited galaxy sample for the A realisation...")
-    create_catalogue(lightcone_dir, files, filename, multiplier)
+    create_catalogue(lightcone_dir, files, filename)
     print(f"Galaxy catalogue created, elapsed time: {datetime.now() - start_time}")
 
     start_time = datetime.now()
     lightcone_dir = "/freya/ptmp/mpa/vrs/TestRuns/MTNG/MTNG-L500-2160-B/SAM/galaxies_lightcone_01"
     filename = "magnitude_limited_B.hdf5"
     print("Creating magnitude limited galaxy sample for the B realisation...")
-    create_catalogue(lightcone_dir, files, filename, multiplier)
+    create_catalogue(lightcone_dir, files, filename)
     print(f"Galaxy catalogue created, elapsed time: {datetime.now() - start_time}")
